2217bcb371af9ec80259fade283ceb665cc8d2f6
Holzmusik <lts-elektronik@t-online.de>
2026-04-15 00:40:18 +0200

    Update postroot.sh

--- postroot.sh
+++ postroot.sh
@@ -9,7 +9,7 @@ APPDIR=$LBHOMEDIR/bin/plugins/$PLUGINNAME
 WEBDIR=$LBHOMEDIR/webfrontend/html/plugins/$PLUGINNAME
 GITURL="https://github.com/lox-audioserver/lox-audioserver.git"
 #GITURL="https://github.com/mr-manuel/Loxone_lox-audioserver.git"
-GITBRANCH="beta"
+GITBRANCH="testing"
 LOCALIMG="lox-audioserver:beta-local"
 
 
